fded4e6ee13337561e45c6188b2a9d0025cd645c
paras-gem <germanium074@gmail.com>
2026-06-05 18:04:43 +0530

    package.json updates, route.js deleted and manifest file updates

--- app/manifest.js
+++ app/manifest.js
@@ -1,1 +1,9 @@
-
+export default function manifest() {
+  return {
+    name: 'PuffyWalls',
+    short_name: 'PuffyWalls',
+    description: 'A wallpaper app',
+    start_url: '/',
+    display: 'standalone',
+  };
+}
